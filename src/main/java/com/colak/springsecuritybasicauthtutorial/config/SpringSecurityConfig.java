387bd13428f1067a3788bb3b09bfdc672eeeed88
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-06 10:22:48 +0300

    JdbcUserDetailsManager example

--- src/main/java/com/colak/springsecuritybasicauthtutorial/config/SpringSecurityConfig.java
+++ src/main/java/com/colak/springsecuritybasicauthtutorial/config/SpringSecurityConfig.java
@@ -6,14 +6,21 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -23,20 +30,30 @@ public class SpringSecurityConfig {
     @Order(1)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                        authorizeHttpRequestsCustomizer -> {
-                            authorizeHttpRequestsCustomizer.requestMatchers("/api/v1/secured/**").authenticated();
-                            authorizeHttpRequestsCustomizer.anyRequest().permitAll();
+                        customizer -> {
+                            customizer.requestMatchers(antMatcher("/h2-console/*")).permitAll();
+                            customizer.requestMatchers("/api/v1/secured/**").authenticated();
+                            customizer.anyRequest().permitAll();
                         })
                 .httpBasic(Customizer.withDefaults());
 
-        http.csrf(csrf ->
-                        csrf
-                                .ignoringRequestMatchers("/api/v1/secured/**")
-                                .csrfTokenRepository(new CookieCsrfTokenRepository())
+        enableH2Console(http);
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        http.csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/v1/secured/**")
+                        .ignoringRequestMatchers("/h2-console/*")
+                        .csrfTokenRepository(new CookieCsrfTokenRepository())
                 )
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.NEVER));
 
         return http.build();
+    }
+
+    // See https://stackoverflow.com/questions/53395200/h2-console-is-not-showing-in-browser
+    private void enableH2Console(HttpSecurity http) throws Exception {
+        // This allows the page to be displayed in a frame on the same origin as the page itself
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
     }
 
     @Bean
@@ -44,14 +61,26 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+    // This is not a bean. I am keeping it as reference only
     public InMemoryUserDetailsManager userDetailsService() {
+        String password = passwordEncoder().encode("password");
 
         UserDetails user = User.builder()
                 .username("user")
-                .password(passwordEncoder().encode("password"))
+                .password(password)
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jUserDetailsManager.setUsersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?;");
+
+        jUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT username, authority FROM authorities WHERE username = ?;");
+
+        return jUserDetailsManager;
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+create table users(
+  username varchar_ignorecase(50) not null primary key,
+  password varchar_ignorecase(500) not null,
+  enabled boolean not null
+  );
+
+create table authorities (
+  username varchar_ignorecase(50) not null,
+  authority varchar_ignorecase(50) not null,
+  constraint fk_authorities_users foreign key(username) references users(username)
+  );
+
+create unique index ix_auth_username on authorities (username,authority);
+
+--password is 'password' in bcrypt encoding
+INSERT INTO users (username, password, enabled) VALUES
+    ('user', '$2a$10$8fwn0LUKql6wTzJHO2QoQ.Nd.59eIyFwaucgBJoiZ/T5SqrqNmyBm', TRUE);
+
+INSERT INTO authorities (username, authority)
+VALUES ('user', 'ROLE_USER');
